3204b7974b00d0a7e5dbae48e515f92f3b663c2f
Richard Wilbur <richard.wilbur@hotmail.com>
2018-08-26 18:34:12 -0700

    Initial adding of redux

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+
+// 2: We can create an initial store with a dummy reducer, and no state or middleware
+const store = createStore(() => [], {}, applyMiddleware());
+
+// 1: We initially had the App at the top
+// ReactDOM.render(<App />, document.querySelector('#root'));
+
+// 3: Add the Provider above the App component
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
